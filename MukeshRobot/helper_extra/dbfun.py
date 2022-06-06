3b4c460c90fb5afd2009973646d57ac9838b6390
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-06 12:32:26 +0530

    Add files via upload

--- MukeshRobot/helper_extra/dbfun.py
+++ MukeshRobot/helper_extra/dbfun.py
@@ -1,1 +1,51 @@
+from MukeshRobot import MONGO_DB_URI
+from typing import Dict, List, Union
+from pymongo import MongoClient
 
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client["MukeshRobot"]
+
+coupledb = db.couple
+
+
+async def _get_lovers(chat_id: int):
+    lovers = coupledb.find_one({"chat_id": chat_id})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    coupledb.update_one({"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True)
+
+
+async def int_to_alpha(user_id: int) -> str:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    text = ""
+    user_id = str(user_id)
+    for i in user_id:
+        text += alphabet[int(i)]
+    return text
+
+
+async def alpha_to_int(user_id_alphabet: str) -> int:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    user_id = ""
+    for i in user_id_alphabet:
+        index = alphabet.index(i)
+        user_id += str(index)
+    user_id = int(user_id)
+    return user_id
