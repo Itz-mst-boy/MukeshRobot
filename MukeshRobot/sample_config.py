e80d8b888b3499c353ceb7dfd8c784d675525baa
Itz-mst-boy <Itz-mst-boy@users.noreply.github.com>
2022-06-06 07:27:42 +0000

    Automated code formatting
    Signed-off-by: AnonymousR1025 <102808014+AnonymousR1025@users.noreply.github.com>

--- MukeshRobot/sample_config.py
+++ MukeshRobot/sample_config.py
@@ -17,9 +17,11 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5142140533 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5142140533  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "anonymous_was_bot"
-    SUPPORT_CHAT = "worldwide_friend_zone"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = (
+        "worldwide_friend_zone"  # Your own group for support, do not add the @
+    )
     JOIN_LOGGER = (
         -1001613182747
     )  # Prints any new group the bot is added to, prints just the name and ID.
