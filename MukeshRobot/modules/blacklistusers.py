310faf46a156727309dd580e0dc946971bd329e6
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-07 14:58:26 +0530

    Update blacklistusers.py

--- MukeshRobot/modules/blacklistusers.py
+++ MukeshRobot/modules/blacklistusers.py
@@ -165,5 +165,5 @@ dispatcher.add_handler(BL_HANDLER)
 dispatcher.add_handler(UNBL_HANDLER)
 dispatcher.add_handler(BLUSERS_HANDLER)
 
-__mod_name__ = "Blacklisting Users"
+__mod_name__ = "ʙʟᴀᴄᴋʟɪsᴛɪɴɢ ᴜsᴇʀs👥"
 __handlers__ = [BL_HANDLER, UNBL_HANDLER, BLUSERS_HANDLER]
