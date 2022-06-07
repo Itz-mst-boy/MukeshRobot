67a5eed67f42fc0fbbffd26dc8e863986bd6eb4c
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-07 21:00:23 +0530

    Update fonts.py

--- MukeshRobot/modules/fonts.py
+++ MukeshRobot/modules/fonts.py
@@ -486,7 +486,7 @@ __help__ = """
  ❍ /latin *:* Change the text into latin text.
  ❍ /lined *:* Change the text into lined text.
  """
-__mod_name__ = "Fᴏɴᴛ Eᴅɪᴛᴏʀ"
+__mod_name__ = "📝FᴏɴᴛEᴅɪᴛᴏʀ📝"
 
 WEEBIFY_HANDLER = DisableAbleCommandHandler("weebify", weebify)
 BUBBLE_HANDLER = DisableAbleCommandHandler("bubble", bubble)
