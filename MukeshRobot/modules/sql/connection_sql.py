264ae2e98ea52b85169f1fceaed98eaca63f7375
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-07 06:54:13 +0530

    Update connection_sql.py

--- MukeshRobot/modules/sql/connection_sql.py
+++ MukeshRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger
 
-from FallenRobot.modules.sql import SESSION, BASE
+from MukeshRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
