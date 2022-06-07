743e8ebd85799149de309a828cb7d2f93b65df2a
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-07 20:28:51 +0530

    Update notes.py

--- MukeshRobot/modules/notes.py
+++ MukeshRobot/modules/notes.py
@@ -542,7 +542,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "Nᴏᴛᴇs"
+__mod_name__ = "📚Nᴏᴛᴇs📚"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
