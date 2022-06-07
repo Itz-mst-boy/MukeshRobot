d93fb8215acdc94bd69a73999db646f243f2a80d
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-07 21:12:33 +0530

    Update stickers.py

--- MukeshRobot/modules/stickers.py
+++ MukeshRobot/modules/stickers.py
@@ -456,7 +456,7 @@ __help__ = """
  ❍ /stickers*:* Find stickers for given term on combot sticker catalogue
 """
 
-__mod_name__ = "🥀Sᴛɪᴄᴋᴇʀ🥀"
+__mod_name__ = "🔺Sᴛɪᴄᴋᴇʀ🔺"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
 KANG_HANDLER = DisableAbleCommandHandler("kang", kang, admin_ok=True)
