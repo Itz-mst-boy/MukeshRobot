7b03f343de454dc9a62e7d8b7acb94a208bb575d
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-26 05:05:21 +0530

    Update bug.py

--- MukeshRobot/modules/bug.py
+++ MukeshRobot/modules/bug.py
@@ -47,7 +47,7 @@ async def bug(_, msg: Message):
     datetimes_fmt = "%d-%m-%Y"
     datetimes = datetime.utcnow().strftime(datetimes_fmt)
 
-    thumb = "https://telegra.ph/file/40eb1ed850cdea274693e.jpg"
+    thumb = "https://telegra.ph/file/141146027deaaefbf7f5a.jpg"
 
     bug_report = f"""
 **#ʙᴜɢ : ** **@{owner_usn}**
@@ -124,4 +124,4 @@ __help__ = """
 *ғᴏʀ ʀᴇᴩᴏʀᴛɪɴɢ ᴀ ʙᴜɢ ɪɴ ᴍᴜᴋᴇsʜ ✘ ʀᴏʙᴏᴛ*
  ❍ /bug *:* ᴛᴏ ʀᴇᴩᴏʀᴛ ᴀ ʙᴜɢ ᴀᴛ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ.
 """
-__mod_name__ = "🔥Bᴜɢ🔥"
+__mod_name__ = "Bᴜɢ🔥"
