a63c0fbb8a8c40290aa0c28d5e790eed44e9261a
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-21 15:54:31 +0530

    Update debug.py

--- MukeshRobot/modules/debug.py
+++ MukeshRobot/modules/debug.py
@@ -66,6 +66,6 @@ dispatcher.add_handler(LOG_HANDLER)
 DEBUG_HANDLER = CommandHandler("debug", debug)
 dispatcher.add_handler(DEBUG_HANDLER)
 
-__mod_name__ = "🚦ᴅᴇʙᴜɢ🚦"
+__mod_name__ = "ᴅᴇʙᴜɢ🚦"
 __command_list__ = ["debug"]
 __handlers__ = [DEBUG_HANDLER]
