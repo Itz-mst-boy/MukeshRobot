e3e6d2910c533b9056a56521a9b1a750bcf761c7
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-21 15:53:27 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -678,7 +678,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "✨ᴅᴇᴠs✨"
+__mod_name__ = "ᴅᴇᴠs❦︎"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
