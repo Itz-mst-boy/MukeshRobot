8068fd8869bb56493c8aa8e7a8cb71f0cc873709
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-21 15:55:27 +0530

    Update dbcleanup.py

--- MukeshRobot/modules/dbcleanup.py
+++ MukeshRobot/modules/dbcleanup.py
@@ -146,5 +146,5 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*")
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ = "🚦ᴅʙ🚦"
+__mod_name__ = "ᴅʙ🚦"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
