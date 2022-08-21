bc68678d4a76ca1fdb3a5171d550c6651ee2e9a3
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-21 15:27:49 +0530

    Update rules.py

--- MukeshRobot/modules/rules.py
+++ MukeshRobot/modules/rules.py
@@ -161,7 +161,7 @@ __help__ = """
  ‣ `/clearrules`*:* clear the rules for this chat.
 """
 
-__mod_name__ = "🚦Rᴜʟᴇs🚦"
+__mod_name__ = "Rᴜʟᴇs🚦"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules)
